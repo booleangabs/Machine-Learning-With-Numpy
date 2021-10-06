d6e3951f79a88fae5438a7b9ec47a0ced761dd72
booleangabs <62074916+booleangabs@users.noreply.github.com>
2021-10-06 02:48:05 -0300

    Update losses.py

--- losses.py
+++ losses.py
@@ -2,3 +2,9 @@ import numpy as np
 
 def MSE(y_true: np.array, y_pred: np.array) -> float:
     return np.round(((y_true - y_pred)**2).mean(axis=0), 5)
+
+def MAE(y_true: np.array, y_pred: np.array) -> float:
+    return np.round((np.abs(y_true - y_pred)).mean(axis=0), 5)
+
+def RMSE(y_true: np.array, y_pred: np.array) -> float:
+    return MSE(y_true, y_pred)**0.5
