6ba9cc1e63459532db73a395fe55681d6c98b4a8
booleangabs <62074916+booleangabs@users.noreply.github.com>
2021-10-09 03:01:38 -0300

    ROC, AUC-ROC and ROC plotting

--- metrics.py
+++ metrics.py
@@ -1,4 +1,7 @@
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set()
 
 def accuracy(y_true: np.array, y_pred: np.array):
     correct = (y_true == y_pred).sum()
@@ -9,6 +12,11 @@ def recall(y_true: np.array, y_pred: np.array):
     tp = TP(y_true, y_pred)
     return tp / (tp + fn)
 
+def falsePositiveRate(y_true: np.array, y_pred: np.array):
+    fp = FP(y_true, y_pred)
+    tn = TN(y_true, y_pred)
+    return fp / (fp + tn)
+
 def precision(y_true: np.array, y_pred: np.array):
     tp = TP(y_true, y_pred)
     fp = FP(y_true, y_pred)
@@ -16,6 +24,29 @@ def precision(y_true: np.array, y_pred: np.array):
 
 def f1Score(y_true: np.array, y_pred: np.array):
     return 2 / ((1 / precision(y_true, y_pred)) + (1 / recall(y_true, y_pred)))
+
+def roc(y_true: np.array, probabilities: np.array, n_thresholds: float) -> tuple:
+    tpr = []
+    fpr = []
+    for i in range(n_thresholds + 1):
+        threshold = i / n_thresholds
+        y_pred = (probabilities >= threshold).astype('uint8')
+        tpr.append(recall(y_true, y_pred))
+        fpr.append(falsePositiveRate(y_true, y_pred))
+    return np.array(tpr), np.array(fpr)
+
+def plotROC(tpr: np.array, fpr: np.array, n_thresholds: int):
+    figure = plt.figure()
+    axis = figure.add_subplot()
+    axis.set_title(f"ROC - AUC = {AUCROC(tpr, fpr, n_thresholds)}")
+    plt.scatter(fpr, tpr)
+    plt.show()
+
+def AUCROC(tpr: np.array, fpr: np.array, n_thresholds: int) -> float:
+    sum_ = 0
+    for i in range(n_thresholds):
+        sum_ += (fpr[i] - fpr[i + 1]) * tpr[i]
+    return np.round(sum_, 3)
 
 def confusionMatrix(y_true: np.array, y_pred: np.array):
     pass
