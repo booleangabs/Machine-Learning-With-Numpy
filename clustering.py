661e3fd0bf9dc4063dc340963ed8df304aad4c8c
booleangabs <62074916+booleangabs@users.noreply.github.com>
2021-10-15 09:46:49 -0300

    KMeans fixed + automatic cluster number chooser

--- clustering.py
+++ clustering.py
@@ -27,48 +27,70 @@ class KMeans:
                 counts[argmin_dist] += 1
             means = (means.T / counts).T
             self.centroids = means
-            if last == self.labels:
+            if np.all(last == self.labels):
                 break
             last = self.labels
-    
+        self.inertia = self.__getInertia()
+        
     def predict(self, X_new: np.array, n_neighbours: int=3):
         classifier = KNN(n_neighbours)
         classifier.fit(self.X, self.labels)
         return classifier.predict(X_new)
     
     def __initCentroids(self, X: np.array, k: int):
-        centroids = np.array([])
-        for i in range(k):
-            if i == 0:
-                np.append(centroids, X[np.random.randint(0, X.shape[0])])
-                continue
+        centroids = []
+        centroids.append(X[np.random.randint(0, X.shape[0])])
+        for _ in range(k - 1):
             probabilities = self.__computeCentroidProb(centroids, X)
             new_centroid = X[np.random.choice(np.arange(0, X.shape[0]), p=probabilities)]
-            np.append(centroids, new_centroid)
+            centroids.append(new_centroid)
         self.centroids = centroids
             
-    def __distance(self, x, y):
+    def __distance(self, x, y) -> float:
             return ((x - y).T.dot(x - y))
         
-    def __distances(self, p: np.array, M: np.array):
-        return np.float32([self.distance(p, i) for i in M])
+    def __distances(self, p: np.array, M: np.array) -> np.float32:
+        return np.float32([self.__distance(p, i) for i in M])
     
-    def __computeCentroidProb(self, centroids: np.array, X: np.array):
-        min_distances = np.array([])
+    def __computeCentroidProb(self, centroids: list, X: np.array) -> np.float32:
+        min_distances = []
         for x in X:
-            distances = [self.distance(x, i) for i in centroids]
-            np.append(min_distances, min(distances))
+            distances = [self.__distance(x, i) for i in centroids]
+            min_distances.append(min(distances))
+        min_distances = np.float32(min_distances)
         return min_distances / min_distances.sum()
     
+    def __getInertia(self) -> float:
+        classes = {i:self.X[self.labels == i] for i in range(self.n_clusters)}
+        inertia = 0
+        for i in classes:
+            inertia += (self.__distances(self.centroids[i], classes[i])).sum()
+        return inertia
+    
 class KChooser:
-    def __init__(self, inertia: np.array, max_n: int=20):
-        self.inertia = inertia
+    def __init__(self, max_n: int=20):
         self.max_n = max_n
+        self.chosen_n = -1
         
-    def __call__(self):
-        for i in range(2, self.max_n - 1):
-            p = (self.inertia[i] / self.inertia[i + 1]) - (self.inertia[i] / self.inertia[i - 1])
-            if 0 <= p <= 1:
+    def __call__(self, X: np.array, iterations: int=3):
+        ns = []
+        inertias = np.zeros((iterations, self.max_n - 1))
+        threshold = 1 / (1 + ((iterations / 10) - 3 / 10))
+        for i in range(iterations):
+            chosen_n, inertias[i] = self.__call(X, threshold)
+            ns.append(chosen_n)
+        self.average_inertias = inertias.mean(0)
+        self.chosen_n = int(np.array(ns).mean()) + 1
+            
+    def __call(self, X: np.array, threshold: float=1):
+        inertias = []
+        for n in range(1, self.max_n):
+            kmeans = KMeans(n)
+            kmeans.fit(X, 3000)
+            inertias.append(kmeans.inertia)
+        for i in range(1, self.max_n - 2):
+            p = (inertias[i] / inertias[i + 1]) - (inertias[i] / inertias[i - 1])
+            if 0 < p < threshold:
                 break
-        return i + 1
+        return i, np.array(inertias)
     
