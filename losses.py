8701285ef7447091fa4b3199ed91eb1402561689
booleangabs <62074916+booleangabs@users.noreply.github.com>
2021-10-29 16:38:38 -0300

    Hinge Loss

--- losses.py
+++ losses.py
@@ -13,8 +13,11 @@ def MAE(y_true: np.array, y_pred: np.array) -> float:
     return np.round((np.abs(y_true - y_pred)).mean(axis=0), 5)
 
 def RMSE(y_true: np.array, y_pred: np.array) -> float:
-    return MSE(y_true, y_pred)**0.5
+    return np.round(MSE(y_true, y_pred)**0.5, 5)
 
-def logloss(y_true: np.array, y_pred: np.array) -> float:
-    pred = (y_pred * (1 - 2 * 1e-15)) + 1e-15 # clip to [1e-15, 1 - 1e-15]
-    return -((y_true * np.log(pred)) + ((1 - y_true) * np.log(1 - pred))).mean()
+def logLoss(y_true: np.array, y_pred: np.array) -> float:
+    pred = (y_pred * (1 - 2 * 1e-15)) + 1e-15
+    return np.round(-((y_true * np.log(pred)) + ((1 - y_true) * np.log(1 - pred))).mean(axis=0), 5)
+
+def hingeLoss(y_true, y_pred):
+    return np.round(np.clip(1 - y_true * y_pred, 0, float('inf')).mean(axis=0), 5)
