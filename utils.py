230035eb2e22db555342404a30a2cb62ef40b763
booleangabs <62074916+booleangabs@users.noreply.github.com>
2021-10-29 16:26:51 -0300

    Relu activation

--- utils.py
+++ utils.py
@@ -16,29 +16,34 @@ class Algorithm:
         raise NotImplementedError()
         
 class Activation:
-    def __init__(self):
-        pass
-    
+    def __init__(self, grad: bool=False):
+        self.grad = grad
+        
     def __call__(self):
         raise NotImplementedError()
     
-    def grad(self):
+    def __grad(self):
         raise NotImplementedError()
         
 class Sigmoid(Activation):
-    def __init__(self, grad: bool=False):
-        self.grad = grad
-    
     def __call__(self, z):
-        return self.__sigmoid(z) if self.grad else self.__grad(z)
+        return self.__grad(z) if self.grad else self.__sigmoid(z)
     
     def __sigmoid(self, z):
-        return np.round(1 / (1 + np.exp(z)), 5)
+        return np.round(1 / (1 + np.exp(-z)), 5)
     
     def __grad(self, z):
         return self.__sigmoid(z) * (1 - self.__sigmoid(z))
     
 
 class Relu(Activation):
-    def __init__(self):
-        pass
+    def __call__(self, x):
+        return self.__grad(x) if self.grad else self.__relu(x)
+    
+    def __relu(self, x):
+        return np.clip(x, 0, float('inf'))
+    
+    def __grad(self, x):
+        grad = np.zeros_like(x)
+        grad[x > 0] = 1
+        return grad
