9c85ad9b259484c180871ef8748b677468c712cc
booleangabs <62074916+booleangabs@users.noreply.github.com>
2021-10-29 16:09:24 -0300

    Support for multivariate linear regression

--- regression.py
+++ regression.py
@@ -9,40 +9,15 @@ from utils import Algorithm
 
         
 class LinearRegression(Algorithm):
-    def __init__(self, mode: str):
-        assert mode in ('Simple', 'Multivariate')
-        self.mode = mode
+    def __init__(self):
+        pass
         
-    def fit(self, X_train: np.array, y_train: np.array, alpha=1e-3, max_iter=1000, patience=5):
-        if self.mode == "Simple":
-            assert (0 < alpha <= 1)
-            self.history = {}
-            self.w0, self.w1 = np.random.randn(2)
-            current_pred = np.zeros_like(y_train)
-            last_loss = float('inf')
-            ticker = 0
-            for i in range(max_iter):
-                if ticker > patience:
-                    break
-                current_pred = self.w0 * X_train + self.w1
-                diff = (y_train - current_pred)
-                dw0 = -2 * np.round((diff * X_train).mean(axis=0), 4)
-                dw1 = -2 * diff.mean(axis=0)
-                self.w0 -= alpha * dw0
-                self.w1 -= alpha * dw1
-                
-                current_loss = MSE(y_train, current_pred)
-                self.history[i] = current_loss
-                if abs(current_loss - last_loss) < 0.1: # Early stopping
-                    ticker += 1 
-                last_loss = current_loss
-        else:
-            moore_penrose = np.linalg.inv(X_train.T @ X_train) @ X_train.T
-            self.w = moore_penrose @ y_train
+    def fit(self, X_train: np.array, y_train: np.array):
+        X_train = np.hstack((X_train, np.ones((X_train.shape[0], 1))))
+        moore_penrose = np.linalg.inv(X_train.T @ X_train) @ X_train.T
+        self.W = moore_penrose @ y_train
+        self.train_score = MSE(y_train, self.predict(X_train))
             
     def predict(self, X: np.array) -> np.array:
-        if self.mode == "Simple":
-            prediction = self.w0*X.flatten() + self.w1
-        else:
-            prediction = (self.w * X).sum(axis=1)
-        return prediction
+        X = np.hstack((X, np.ones((X.shape[0], 1))))
+        return X.dot(self.W) + self.b if self.mode == 0 else X.dot(self.W)
