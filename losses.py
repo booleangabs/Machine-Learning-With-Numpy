34c5eee41a97611379b7a512a9227ab7d82b087c
booleangabs <62074916+booleangabs@users.noreply.github.com>
2021-10-29 16:08:06 -0300

    Log loss

--- losses.py
+++ losses.py
@@ -14,3 +14,7 @@ def MAE(y_true: np.array, y_pred: np.array) -> float:
 
 def RMSE(y_true: np.array, y_pred: np.array) -> float:
     return MSE(y_true, y_pred)**0.5
+
+def logloss(y_true: np.array, y_pred: np.array) -> float:
+    pred = (y_pred * (1 - 2 * 1e-15)) + 1e-15 # clip to [1e-15, 1 - 1e-15]
+    return -((y_true * np.log(pred)) + ((1 - y_true) * np.log(1 - pred))).mean()
