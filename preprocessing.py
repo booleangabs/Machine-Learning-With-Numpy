5beb93659907a9ee9e4805f3535ebbf7ddd6f799
booleangabs <62074916+booleangabs@users.noreply.github.com>
2021-10-29 16:08:50 -0300

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -9,21 +9,30 @@ import numpy as np
 from utils import Constants as cts
 
 
-class Scaler:
+class StandardScaler:
     def __init__(self, mode: int):
         assert mode in (0, 1), f"{mode} is not a valid value for mode."
         self.mode = mode
         
-    def transform(self, X: np.array):
+    def fit(self, X: np.array):
         if self.mode == cts.PPR_SCALE_MINMAX:
-            X_max = X.max(0)
-            X_min = X.min(0)
-            X_transformed = np.true_divide((X - X_min), (X_max - X_min))
+            self.X_max = X.max(0)
+            self.X_min = X.min(0)
         else:
-            X_mean = X.mean(axis=0)
-            X_std = X.std(axis=0)
+            self.X_mean = X.mean(axis=0)
+            self.X_std = X.std(axis=0)
+        
+    def transform(self, X: np.array) -> np.array:
+        if self.mode == cts.PPR_SCALE_MINMAX:
+            X_transformed = np.true_divide((X - self.X_min), (self.X_max - self.X_min))
+        else:
             with np.errstate(divide='ignore', invalid='ignore'):
-                X_transformed = np.true_divide(X - X_mean, X_std)
+                X_transformed = np.true_divide(X - self.X_mean, self.X_std)
                 X_transformed[X_transformed == np.inf] = 0
         return np.nan_to_num(X_transformed, nan=0.0, posinf=0, neginf=0)
-            
+
+    def fit_transform(self, X: np.array) -> np.array:
+        self.fit(X)
+        return self.transform(X)
+
+class min
