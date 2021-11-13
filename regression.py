23c9b492d07c301b50b1e5aaec6c09a6ea6136a3
booleangabs <62074916+booleangabs@users.noreply.github.com>
2021-11-12 23:41:22 -0300

    Update regression.py

--- regression.py
+++ regression.py
@@ -91,3 +91,7 @@ class RidgeRegression(Algorithm):
     def predict(self, X: np.array) -> np.array:
         X = np.hstack((X, np.ones((X.shape[0], 1))))
         return X.dot(self.W)
+    
+    
+class ElasticNet(Algorithm):
+    pass
