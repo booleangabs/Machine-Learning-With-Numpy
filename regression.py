4f8df00f629a33c6177d596c0fbdf40a6654729a
booleangabs <62074916+booleangabs@users.noreply.github.com>
2021-11-20 12:51:39 -0300

    Updating Lasso and Ridge regression + addition of Elastic Net Regression

--- regression.py
+++ regression.py
@@ -6,6 +6,7 @@ import numpy as np
 # Locals
 from losses import MSE
 from utils import Algorithm
+from regularizers import Regularizer, Placeholder, L1, L2, ElasticNet
 
         
 class LinearRegression(Algorithm):
@@ -23,10 +24,14 @@ class LinearRegression(Algorithm):
         return X.dot(self.W)
 
 class GradientDescentRegression(Algorithm):
-    def __init__(self, lr: float=1e-5, epochs: int=100):
+    def __init__(self, lr: float=1e-5, epochs: int=100, regularizer: Regularizer=None):
         self.lr = lr
         self.epochs = epochs
-        
+        if not regularizer:
+            self.regularizer = Placeholder()
+        else:
+            self.regularizer = regularizer
+            
     def fit(self, X_train: np.array, y_train: np.array):
         X_train = np.hstack((X_train, np.ones((X_train.shape[0], 1))))
         self.history = {}
@@ -36,62 +41,34 @@ class GradientDescentRegression(Algorithm):
             current_pred = X_train.dot(self.W)
             diff = (current_pred - y_train)
             dW = X_train.T.dot(diff) * (2 / n_rows)
-            self.W -= self.lr * dW
-            
-            self.history[i] = MSE(y_train, current_pred)
+            self.W -= self.lr * dW + self.regularizer.grad(self.W)
+
+            self.history[i] = ((y_train - current_pred)**2 + self.regularizer(self.W)).mean()
         
     def predict(self, X: np.array) -> np.array:
         X = np.hstack((X, np.ones((X.shape[0], 1))))
         return X.dot(self.W)
-    
-class LassoRegression(Algorithm):
-    def __init__(self, lr: float=1e-5, lambda_: float=5e-2, epochs: int=100):
-        self.lr = lr
+
+class LassoRegression(GradientDescentRegression):
+    def __init__(self, lambda_: float, lr: float=1e-5, epochs: int=100):
+        self.regularizer = L1(lambda_)
         self.lambda_ = lambda_
-        self.epochs = epochs
+        super(LassoRegression, self).__init__(lr=lr, 
+                                              epochs=epochs, 
+                                              regularizer=self.regularizer)
         
-    def fit(self, X_train: np.array, y_train: np.array):
-        X_train = np.hstack((X_train, np.ones((X_train.shape[0], 1))))
-        self.history = {}
-        n_rows, n_columns = X_train.shape
-        self.W = np.random.uniform((1 / -n_columns), 1 / n_columns, (n_columns,))
-        for i in range(self.epochs):
-            current_pred = X_train.dot(self.W)
-            diff = (current_pred - y_train)
-            dW = X_train.T.dot(diff) * (2 / n_rows) + self.lambda_ * np.sign(self.W)
-            self.W -= self.lr * dW
-            
-            W_norm = np.linalg.norm(self.W)
-            self.history[i] = ((y_train - current_pred)**2 + self.lambda_ * W_norm).mean()
-        
-    def predict(self, X: np.array) -> np.array:
-        X = np.hstack((X, np.ones((X.shape[0], 1))))
-        return X.dot(self.W)
-    
-class RidgeRegression(Algorithm):
-    def __init__(self, lr: float=1e-5, omega_: float=5e-2, epochs: int=100):
-        self.lr = lr
+class RidgeRegression(GradientDescentRegression):
+    def __init__(self, omega_: float, lr: float=1e-5, epochs: int=100):
+        self.regularizer = L2(omega_)
         self.omega_ = omega_
-        self.epochs = epochs
+        super(RidgeRegression, self).__init__(lr=lr, 
+                                              epochs=epochs, 
+                                              regularizer=self.regularizer)
         
-    def fit(self, X_train: np.array, y_train: np.array):
-        X_train = np.hstack((X_train, np.ones((X_train.shape[0], 1))))
-        self.history = {}
-        n_rows, n_columns = X_train.shape
-        self.W = np.random.uniform((1 / -n_columns), 1 / n_columns, (n_columns,))
-        for i in range(self.epochs):
-            current_pred = X_train.dot(self.W)
-            diff = (current_pred - y_train)
-            dW = X_train.T.dot(diff) * (2 / n_rows) + 2 * self.omega_ * self.W
-            self.W -= self.lr * dW
-            
-            W_norm_sq = np.linalg.norm(self.W) ** 2
-            self.history[i] = ((y_train - current_pred)**2 + self.omega_ * W_norm_sq).mean()
-        
-    def predict(self, X: np.array) -> np.array:
-        X = np.hstack((X, np.ones((X.shape[0], 1))))
-        return X.dot(self.W)
-    
-    
-class ElasticNet(Algorithm):
-    pass
+class ElasticNetRegression(GradientDescentRegression):
+    def __init__(self, lambda_1: float, lambda_2: float, lr: float=1e-5, epochs: int=100):
+        self.regularizer = ElasticNet(lambda_1, lambda_2)
+        self.lambdas = (lambda_1, lambda_2)
+        super(ElasticNetRegression, self).__init__(lr=lr, 
+                                              epochs=epochs, 
+                                              regularizer=self.regularizer)
